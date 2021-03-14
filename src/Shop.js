64e11c9e3c7610b92199266cc5ad6052e0012645
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-13 20:32:14 -0600

    pulled from shopping api to dynamically create routes

--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function Shop() {
-  return (
+
+    // to run
+    // this will only run when the component mounts b/c of the =>  , []
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    // added async b/c its coming from a database
+    const fetchItems = async () => {
+        const data = await fetch('https://fakestoreapi.com/products')
+        // .then(res=>res.json())
+        // .then(json=>console.log(json));
+
+        const items = await data.json();
+        console.log(items);
+    }
+    return (
     <div>
         <h1>Shop Page</h1>
     </div>
-  );
+    );
 }
 
 export default Shop;
